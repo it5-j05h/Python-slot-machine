60b42d0255d3258f7c2b4be41ff1a0b5afc2365a
Joshua <joshua.rogers.2105@gmail.com>
2022-11-06 07:30:14 +0000

    core works!
    implementation of the core idea working, scoring system working

--- main.py
+++ main.py
@@ -14,6 +14,30 @@ symbol_count = {
     "D": 8,
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
+
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
@@ -80,14 +104,13 @@ def get_bet():
            if MIN_BET <= amount <= MAX_BET:
             break
         else:
-            print("Amount must be between £{MIN_BET} - £{MAX_BET}.")
+            print(f"Amount must be between £{MIN_BET} - £{MAX_BET}.")
     else:
         print("Please enter a number.")
 
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -103,5 +126,22 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won £{winnings}.")
+    print(f"you won on lines:", *winning_lines)
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is £{balance}")
+        answer = input("Press enter to play (q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with £{balance}.")
+   
+
 
 main()
